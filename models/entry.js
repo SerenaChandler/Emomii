6435f92d4f5288dca420b3582a1d58ea20e0e4ae
Serena Chandler <serenachandler2019@gmail.com>
2021-05-04 15:15:55 -0700

    made models entry, moon, and user

--- models/entry.js
+++ models/entry.js
@@ -1,3 +1,38 @@
-const {Model, Datatypes} = require("sequelize");
-const sequelize = require("../config/connection")
-class Entry 
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Entry extends Model {}
+
+Entry.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Entry",
+  }
+);
+
+module.exports = Entry;
