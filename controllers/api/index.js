2103695e6d79b7996ac3ccb098d0ba5551b36205
Erick Meline <erickmeline@gmail.com>
2021-05-05 16:25:02 -0700

    removed mood ref

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const blogRoutes = require('./post-routes');
 const blogRoutes = require('./user-routes');
 
-router.use('/mood', moodRoutes); // maybe rename this to 'data'?
 router.use('/posts', postRoutes);
 router.use('/user', postRoutes);
 
