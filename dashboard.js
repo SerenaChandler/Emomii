e1e3e4125387a666a1ac6722e65743dad3be4485
Patrick Ly <patrickly72@gmail.com>
2021-05-06 01:48:11 -0700

    added second layer of questions

--- dashboard.js
+++ dashboard.js
@@ -8,6 +8,14 @@ function populate(parentfeelings,childfeelings){
 		var optionArray = ["|","lonely|Lonely", "vunerable|Vunerable", "despair|Despair", "guilty|Guilty", "depressed|Depressed", "hurt|Hurt"];
 	} else if(parentfeelings.value == "Angry"){
 		var optionArray = ["|","letdown|Letdown", "humiliated|Humiliated", "bitter|Bitter", "mad|Mad", "agressive|Agressive", "frustrated|Frustrated", "distant|Distant", "critical|Critical"];
+	} else if(parentfeelings.value == "Surprised"){
+		var optionArray = ["|","startled|Startled", "confused|Confused", "confused|Confused", "amazed|Amazed", "excited|Excited"];
+	} else if(parentfeelings.value == "Bad"){
+		var optionArray = ["|","bored|Bored", "busy|Busy", "stressed|Stressed", "tired|Tired"];
+	} else if(parentfeelings.value == "Fearful"){
+		var optionArray = ["|","scared|Scared", "anxious|Anxious", "insecure|Insecure", "weak|Weak", "rejected|Rejected", "threatened|Threatened"];
+	}  else if(parentfeelings.value == "Disgusted"){
+		var optionArray = ["|","disapproving|Disapproving", "disenchanted|Disenchanted", "awful|Awful", "repelled|Repelled"];
 	}
 	for(var option in optionArray){
 		var pair = optionArray[option].split("|");
@@ -17,6 +25,28 @@ function populate(parentfeelings,childfeelings){
 		childfeelings.options.add(newOption);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// `disapproving
+// disappointed
+// awful
+// repelled`
+
 
 // `playful
 // content
@@ -60,8 +90,3 @@ function populate(parentfeelings,childfeelings){
 // weak
 // rejected
 // threatened`
-
-// `disapproving
-// disappointed
-// awful
-// repelled`
