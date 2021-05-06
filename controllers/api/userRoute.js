998e217e164ab3c522d965d558a00dc902176d82
Serena Chandler <serenachandler2019@gmail.com>
2021-05-05 17:13:02 -0700

    changed routes to avoid conflict

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -1,1 +1,54 @@
-1
+1const router = require('express').Router();
+const { User, Blog, Comment } = require('../../models');
+
+router.post('/', (req, res) => {
+	User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+	}).then(response => {
+		req.session.save(() => {
+			req.session.user_id = response.id;
+			req.session.name = response.name;
+			req.session.logged_in = true;
+			res.json(response);
+		});
+	});
+});
+
+router.post('/login', (req, res) => {
+	User.findOne({
+		where: {
+			email: req.body.email
+		}
+	}).then(response => {
+		if (!response) {
+			res.status(400).json({ message: `${req.body.email} not found` });
+			return;
+		}
+		const validPassword = response.checkPassword(req.body.password);
+		if (!validPassword) {
+			res.status(400).json({ message: 'Invalid password' });
+			return;
+		}
+		req.session.save(() => {
+			req.session.user_id = response.id;
+			req.session.name = response.name;
+			req.session.logged_in = true;
+			res.json({ user: response, message: 'logged in' });
+		});
+	});
+});
+
+router.post('/logout', (req, res) => {
+	if (req.session.logged_in) {
+		req.session.destroy(() => {
+			res.status(204).render('homepage').end();
+		});
+	}
+	else {
+		res.status(404).render('homepage').end();
+	}
+});
+
+module.exports = router;
---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const withAuth = require('../utils/auth');
+const { User } = require('../models');
+
+router.get('/', withAuth, (req, res) => {
+	Post.findAll({
+		where: {
+			user_id: req.session.user_id
+		},
+		attributes: [
+		'id',
+		'title',
+		'content',
+        'mood',
+		'created_at'
+		],
+		include: [{
+			model: User,
+			attributes: ['name']
+		}]
+	}).then(response => {
+		const posts = response.map(post => post.get({ plain: true }));
+		res.render('dashboard', { posts, loggedIn: true });
+	}).catch(err => {
+		res.status(500).json(err);
+	});
+});
+
+module.exports = router;
