2f754a3d2a6439e7a3b61e07d93b8c0d24483e5d
Serena Chandler <serenachandler2019@gmail.com>
2021-05-06 09:03:54 -0700

    remade model files for heroku

---
+++ models/Mood.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+class Mood extends Model {}
+
+Mood.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    parentMood: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    childMood: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    grandChildMood: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "User",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "Entry",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Mood",
+  }
+);
+
+module.exports = Mood;
